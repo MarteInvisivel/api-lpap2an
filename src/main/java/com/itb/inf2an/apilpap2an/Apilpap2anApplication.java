0d5d39b05459d1954c11c562d7e4de42361e4f6a
Jenyffer Borges Cavalcanti <rm90389@estudante.fieb.edu.br>
2024-09-24 22:18:35 -0300

    Início Controle de Versão

---
+++ src/main/java/com/itb/inf2an/apilpap2an/Apilpap2anApplication.java
@@ -0,0 +1,13 @@
+package com.itb.inf2an.apilpap2an;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apilpap2anApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Apilpap2anApplication.class, args);
+    }
+
+}
